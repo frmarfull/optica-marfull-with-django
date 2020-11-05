719e8b5282275bc32ede0ba9e15acded6bb38e64
Martín <ma.zamoranoa@alumnos.duoc.cl>
2020-11-04 21:34:17 -0300

    avance

--- optica/producto/views.py
+++ optica/producto/views.py
@@ -3,20 +3,27 @@ from .forms import FormProducto
 from django.contrib import messages
 from .models import Producto
 from django.contrib.auth.decorators import login_required
+from cuenta.models import User
 
 # Create your views here.
 @login_required(login_url='/iniciarSesion/')
 def listarProductos(request):
-    productos = Producto.objects.all()
-    context = {
-        'titulo':'Listar productos',
-        'productos':productos
-    }
-    return render(
-        request,
-        'listar_producto.html',
-        context
-    )
+    if request.user.is_superuser:
+        productos = Producto.objects.all()
+        context = {
+            'titulo':'Listar productos',
+            'productos':productos
+        }
+        return render(
+            request,
+            'listar_producto.html',
+            context
+        )
+    else:
+        return redirect(
+            '/'
+        )
+
 def listadoCatalogo(request):
     productos = Producto.objects.all()
     context = {
@@ -34,53 +41,68 @@ def listadoCatalogo(request):
 
 @login_required(login_url='/iniciarSesion/')
 def agregarProducto(request):
-    formulario= FormProducto()
-    if request.method =='POST':
-        formulario = FormProducto(request.POST,request.FILES)
-        if formulario.is_valid():
-            productoNuevo= formulario.save()
-            messages.add_message(
-                request,
-                messages.SUCCESS,
-                'producto agregado'
-            )
-            return redirect('/productos/listar')
-    context = {
-        'formulario':formulario
-    }
-    return render(
-        request,
-        'agregar.html',
-        context
-    )
+    if request.user.is_superuser:
+        formulario= FormProducto()
+        if request.method =='POST':
+            formulario = FormProducto(request.POST,request.FILES)
+            if formulario.is_valid():
+                productoNuevo= formulario.save()
+                messages.add_message(
+                    request,
+                    messages.SUCCESS,
+                    'producto agregado'
+                )
+                return redirect('/productos/listar')
+        context = {
+            'formulario':formulario
+        }
+        return render(
+            request,
+            'agregar.html',
+            context
+        )
+    else:
+        return redirect(
+            '/'
+        )
 @login_required(login_url='/iniciarSesion/')
 def eliminarProducto(request, id_producto):
-    productoEncontrado = Producto.objects.get(pk=id_producto)
-    productoEncontrado.delete()
+    if request.user.is_superuser:
+        productoEncontrado = Producto.objects.get(pk=id_producto)
+        productoEncontrado.delete()
 
-    return redirect('/productos/listar/')
+        return redirect('/productos/listar/')
+    else:
+        return redirect(
+            '/'
+        )
 
 def editarProducto(request,id_producto):
-    productoEncontrado = Producto.objects.get(pk = id_producto)
-    formulario = None
+    if request.user.is_superuser:
+        productoEncontrado = Producto.objects.get(pk = id_producto)
+        formulario = None
 
-    if request.method == 'POST':
-        formulario = FormProducto(request.POST,request.FILES, instance= productoEncontrado)
-        if formulario.is_valid():
-            formulario.save()
-            return redirect('/productos/listar')
+        if request.method == 'POST':
+            formulario = FormProducto(request.POST,request.FILES, instance= productoEncontrado)
+            if formulario.is_valid():
+                formulario.save()
+                return redirect('/productos/listar')
+        else:
+            formulario = FormProducto(instance= productoEncontrado)
+        context = {
+            'titulo':'Modificar producto',
+            'formulario':formulario
+        }
+
+        return render(
+            request,
+            'modificar_producto.html',
+            context
+        )
     else:
-        formulario = FormProducto(instance= productoEncontrado)
-    context = {
-        'titulo':'Modificar producto',
-        'formulario':formulario
-    }
-
-    return render(
-        request,
-        'modificar_producto.html',
-        context
-    )
+         return redirect(
+            '/'
+        )
 
 def catalogo(request):
     return render(request,'catalogo.html')
